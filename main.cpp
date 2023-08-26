8dc855db0b0e787d5b233700ff0f37adc5843296
NarekHovhannisyan001 <139554962+NarekHovhannisyan001@users.noreply.github.com>
2023-08-26 19:11:19 +0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include "List.h"
 
-//#include <iostream>
-
 int main() {
     MyList<int>list;
     list.push_back(4);
     list.push_back(5);
     list.push_front(3);
-    list.printList();
-    std::cout << "\n";
-    list.remove(1);
-    list.insert(1,3);
-    list.printList();
-    std::cout << list.get(2);
-
+    MyList<int>list1;
+    list1.push_back(3);
+    list1.push_back(3);
+    list1 = list;
+    MyList<int>list2{1,2,3};
+    list2.printList();
+    MyList<int>list3(list2);
+    list3.printList();
+    return 0;
 }
